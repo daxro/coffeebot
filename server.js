c50325aeba1f7f6c6dc4e16078cc3c8363654d09
David Romell <David@Davids-MacBook-Air.local>
2017-01-06 22:06:53 +0100

    clean up

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var Botkit = require('botkit')
-var config = require('./config.js')
-var sendToSlack = require('./lib/sendToSlack.js')
 var express = require('express')
 var bodyParser = require('body-parser')
 var emoji = require('node-emoji')
+
+var brew = require('./lib/brew.js')
+var botWakeSleep = require('./lib/botWakeSleep.js')
 
 var app = express()
 
@@ -24,49 +25,20 @@ app.get('/', function(req, res) {
 
 app.get('/brew/', function(req, res) {
   res.send(emoji.get('coffee'))
-
-  // only execute on requets that includes the code
-  if (req.query.code == config.slack.get_code) {
-    console.log('Correct code.')
-    sendToSlack('I just started brewing. I will be back in six minutes!')
-    setTimeout(function() {
-      sendToSlack('I am done! Come and get some of that coffee.')
-    }, 360000)
-  } else {
-    console.log('Incorrect code.')
-  }
+  var code = req.query.code
+  brew(code)
 })
 
 app.post('/wakeup/', function(req, res) {
+  res.send(emoji.get('coffee'))
   var token = req.body.token
-  if (token == config.slack.wake_token) {
-    console.log('Correct token. Wake bot.')
-    res.send(200)
-    controller.spawn({
-      token: config.slack.token
-    }).startRTM()
-  } else {
-    console.log('Incorrect token. Your provided token is: ' + token)
-    res.send({
-      "response_type": "ephemeral",
-      "text": "Sorry. That didn't work :("
-    })
-    process.exit()
-  }
+  botWakeSleep('wake', token)
 });
 
 app.post('/sleep/', function(req, res) {
+  res.send(emoji.get('coffee'))
   var token = req.body.token
-  if (token == config.slack.sleep_token) {
-    console.log('Correct token. Sleep bot.')
-    process.exit();
-  } else {
-    console.log('Incorrect token. Your provided token is: ' + token)
-    res.send({
-      "response_type": "ephemeral",
-      "text": "Sorry. That didn't work :("
-    })
-  }
+  botWakeSleep('sleep', token)
 });
 
 controller.hears('hello', ['direct_message','direct_mention','mention'], function(bot, message) {
